29b4486b28ca1509b51fd59b485998287c0e70a9
hyebin829 <smilerain779@gmail.com>
2022-05-22 01:39:38 +0900

    :sparkles: highlight searchWord

--- src/routes/MainPage/DropDown/DropDownItem/index.tsx
+++ src/routes/MainPage/DropDown/DropDownItem/index.tsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import styles from '../DropDown.module.scss'
+import { createFuzzyMatcher } from 'routes/MainPage/utils/getConsonantSearch'
 
 interface IDropDownItemProps {
   keyWord: string
@@ -8,14 +9,19 @@ interface IDropDownItemProps {
 }
 
 const DropDownItem = ({ keyWord, searchWord }: IDropDownItemProps) => {
-  // eslint-disable-next-line no-console
-  console.log(searchWord)
+  const regex = createFuzzyMatcher(searchWord.replace(/(\s*)/g, ''))
+  const parts = keyWord.split(regex)
+  const word = keyWord.match(regex)?.slice(1, searchWord.length + 1)
+
   return (
     <li>
       <FontAwesomeIcon icon={faMagnifyingGlass} className={styles.serchIcon} />
-      <div>{keyWord}</div>
+      <span>
+        {parts.map((part, index) =>
+          word?.includes(part) ? <mark key={`${keyWord}-${searchWord}-${index + 1}`}>{part}</mark> : part
+        )}
+      </span>
     </li>
   )
 }
-
 export default DropDownItem
